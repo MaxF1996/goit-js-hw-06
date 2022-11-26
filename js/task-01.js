50325753fb24606e78b987148928a5ad2db85277
Max Fedorchuk <98663185+MaxF1996@users.noreply.github.com>
2022-11-26 18:45:35 +0200

    task 1 done

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const items = document.querySelectorAll('.item');
+const numberOfItems = items.length;
+
+console.log('Number of categories:', numberOfItems);
+
+for (const item of items) {
+    console.log('Category: ', item.querySelector('h2').textContent);
+    console.log('Elements: ', item.querySelectorAll('li').length);
+}
+
