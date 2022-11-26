110b2d1c88985512dc5339236e623a5d869d570e
Max Fedorchuk <98663185+MaxF1996@users.noreply.github.com>
2022-11-26 19:17:27 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsHtml = document.querySelector('#ingredients');
+
+const listIngredients = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+for (const li of listIngredients) {
+  ingredientsHtml.append(li);
+}
