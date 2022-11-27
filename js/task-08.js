1150a7998198a5f9a16724e0ee17f128cce5b259
Max Fedorchuk <98663185+MaxF1996@users.noreply.github.com>
2022-11-27 19:34:06 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const formData = {
+    email: formElements.email.value,
+    password: formElements.password.value,
+  };
+  if (formData.email === '' || formData.password === '') {
+    alert('Пусті поля');
+  } else {
+    console.log(formData);
+    event.currentTarget.reset();
+  }
+});
