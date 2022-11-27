50ff3a8ad23ff09f9e9e929e9397eef364178a1d
Max Fedorchuk <98663185+MaxF1996@users.noreply.github.com>
2022-11-27 19:46:33 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const colorButton = document.querySelector('.change-color');
+
+colorButton.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+});
