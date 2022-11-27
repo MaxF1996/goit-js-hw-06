0a0c53de1960952e1596910fc176f746083095fb
Max Fedorchuk <98663185+MaxF1996@users.noreply.github.com>
2022-11-27 21:40:59 +0200

    fix task 9 and done task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector('#boxes');
+
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+
+const createBoxes = amount => {
+  destroyBoxes();
+
+  amount = +document.querySelector('#controls >[type="number"]').value;
+
+  for (let i = 1, size = 30; i <= amount; i += 1, size += 10) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.append(box);
+  }
+};
+
+const destroyBoxes = () => {
+  while (boxes.firstChild) {
+    boxes.removeChild(boxes.lastChild);
+  }
+};
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
